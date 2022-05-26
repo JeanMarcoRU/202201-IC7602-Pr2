5d0255596fae1e19369d64e48b6cceafbf9e6e43
Jean Marco Rojas <jeanmarco1997@gmail.com>
2022-05-25 19:57:47 -0600

    post ready

--- cserver/UDP_server.c
+++ cserver/UDP_server.c
@@ -261,29 +261,38 @@ int main()
         enc = b64_encode(buffer, bytes_read);
         printf("encoded: '%s'\n", enc);
 
+        // char *urllink = "http://localhost:443/api/dns_resolver";
         char *datapost = malloc(MAXSIZE);
         sprintf(datapost, "http://localhost:443/api/dns_resolver?data=%s", enc);
 
         CURL *curl;
-        CURLcode response;
+        CURLcode res;
 
+        /* In windows, this will init the winsock stuff */
         curl_global_init(CURL_GLOBAL_ALL);
 
+        /* get a curl handle */
         curl = curl_easy_init();
         if (curl)
         {
+            /* First set the URL that is about to receive our POST. This URL can
+               just as well be a https:// URL if that is what should receive the
+               data. */
             curl_easy_setopt(curl, CURLOPT_URL, datapost);
+            /* Now specify we want to POST data */
+            curl_easy_setopt(curl, CURLOPT_POST, 1L);
+            /* if we do not provide POSTFIELDSIZE, libcurl will strlen() by
+            itself */
+            // curl_easy_setopt(curl, CURLOPT_POSTFIELDSIZE, MAXSIZE - 1);
 
-            response = curl_easy_perform(curl);
+            /* Perform the request, res will get the return code */
+            res = curl_easy_perform(curl);
+            /* Check for errors */
+            if (res != CURLE_OK)
+                fprintf(stderr, "curl_easy_perform() failed: %s\n",
+                        curl_easy_strerror(res));
 
-            if (response != CURLE_OK)
-            {
-                fprintf(stderr, "Request failed: %s\n", curl_easy_strerror(response));
-            }
-            /*else
-            {
-                printf(response);
-            }*/
+            /* always cleanup */
             curl_easy_cleanup(curl);
         }
         curl_global_cleanup();
@@ -325,7 +334,7 @@ int main()
         {
             printf("Error: sendto()");
         }*/
-
+        free(datapost);
         fflush(stdout);
         fclose(f1);
         fclose(f2);
