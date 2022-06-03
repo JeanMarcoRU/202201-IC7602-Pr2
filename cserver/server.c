67b86468553ffbf00006657ce6cd12d6beaee02f
Jean Marco Rojas <jeanmarco1997@gmail.com>
2022-06-02 19:59:42 -0600

    multi clientes listos

--- cserver/server.c
+++ cserver/server.c
@@ -23,9 +23,9 @@
         (byte & 0x01 ? '1' : '0')
 
 #define PORT 53
-#define MAXSIZE 2048
+#define MAXSIZE 5048
 
-pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;	
+pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
 
 const char b64chars[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
 const int b64invs[] = {62, -1, -1, -1, 63, 52, 53, 54, 55, 56, 57, 58,
@@ -35,10 +35,11 @@ const int b64invs[] = {62, -1, -1, -1, 63, 52, 53, 54, 55, 56, 57, 58,
                        29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42,
                        43, 44, 45, 46, 47, 48, 49, 50, 51};
 
-struct ThreadArgs {
+struct ThreadArgs
+{
     int sock;
     struct sockaddr_in client_addr;
-    unsigned char* buffer;
+    unsigned char *buffer;
     int bytes_read;
     int addr_len;
 };
@@ -244,7 +245,8 @@ void generar_paquete(unsigned char *consulta, int qs, int ttl, int ip)
     consulta[qs + 15] = (ip & 0xff000000) >> 24;
 }
 
-void *thread_function(void *arg){
+void *thread_function(void *arg)
+{
     // ThreadArgs
     ThreadArgs args = *(ThreadArgs *)arg;
     int sock = args.sock;
@@ -252,143 +254,39 @@ void *thread_function(void *arg){
     int bytes_read = args.bytes_read;
     unsigned char *buffer = args.buffer;
     struct sockaddr_in client_addr = args.client_addr;
-    FILE*fptr;
+    FILE *fptr;
 
     pthread_mutex_lock(&mutex);
-        fptr = fopen("peticionDNS.bin", "wb");
-        fwrite(buffer, 1, bytes_read, fptr);
-        fclose(fptr);
+    fptr = fopen("peticionDNS.bin", "wb");
+    fwrite(buffer, 1, bytes_read, fptr);
+    fclose(fptr);
 
-        char *enc;
-        size_t out_len;
+    char *enc;
+    size_t out_len;
 
-        if (buffer[2] & 0x01 && (buffer[2] & 0x1e) == 0)
+    if (buffer[2] & 0x01 && (buffer[2] & 0x1e) == 0)
+    {
+        printf("\nLlegó un paquete query estándar.\n");
+        // para consultar elasticsearch se requiere el nombre de dominio del paquete
+        char hostname[MAXSIZE] = {'\0'};
+
+        int i = 12;
+        int k = 0;
+        while (buffer[i] != 0)
         {
-            printf("\nLlegó un paquete query estándar.\n");
-            // para consultar elasticsearch se requiere el nombre de dominio del paquete
-            char hostname[MAXSIZE] = {'\0'};
-
-            int i = 12;
-            int k = 0;
-            while (buffer[i] != 0)
-            {
-                for (int j = i + 1; j <= i + buffer[i]; j++)
-                    hostname[k++] = buffer[j];
-                i += buffer[i] + 1;
-                if (buffer[i] != 0)
-                    hostname[k++] = '.';
-            }
-
-            printf("Domain name: %s\n", hostname);
-
-            char *dataget = malloc(MAXSIZE);
-            sprintf(dataget, "http://elasticsearch:9200/zones/_doc/_search?q=hostname:%s", hostname);
-
-            fptr = fopen("elasticsearch.json", "w");
-
-            CURL *curl;
-            CURLcode res;
-            curl_global_init(CURL_GLOBAL_ALL);
-            curl = curl_easy_init();
-            if (curl)
-            {
-                curl_easy_setopt(curl, CURLOPT_URL, dataget);
-                curl_easy_setopt(curl, CURLOPT_HTTPGET, 1L);
-                curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, writeCallback);
-                curl_easy_setopt(curl, CURLOPT_WRITEDATA, (void *)fptr);
-                res = curl_easy_perform(curl);
-                if (res != CURLE_OK)
-                    fprintf(stderr, "curl_easy_perform() failed: %s\n",
-                            curl_easy_strerror(res));
-                curl_easy_cleanup(curl);
-            }
-            curl_global_cleanup();
-            
-            fclose(fptr);
-            // if resultado ==
-            fptr = fopen("elasticsearch.json", "r");
-
-            char str_ip[17] = {'\0'};
-            char id[21] = {'\0'};
-            int ttl;
-
-            size_t len = 0;
-            char *texto;
-            ssize_t read;
-
-            read = getline(&texto, &len, fptr);
-            if (texto[2] == 't' && texto[strcspn(texto, "[]") + 1] != ']')
-            { // Esto quiere decir que hay matches
-                fseek(fptr, 0, SEEK_SET);
-                while (1)
-                    if (fgetc(fptr) == ',' && fgetc(fptr) == '"')
-                    {
-                        if ((texto[0] = fgetc(fptr)) == '_' && fgetc(fptr) == 'i' && fgetc(fptr) == 'd' && fgetc(fptr) && fgetc(fptr) && fgetc(fptr))
-                            fgets(id, 21, fptr);
-                        else if (texto[0] == 'T')
-                            break;
-                    }
-
-                fscanf(fptr, "TL\": \"%d\",\"IP\":", &ttl); //     "TTL": "[0-9]+",
-                texto[0] = fgetc(fptr);
-                if (texto[0] == ' ')
-                    fscanf(fptr, "\"%s\"", str_ip);
-                else //texto[0] debería ser una comilla
-                    fscanf(fptr, "%s\"", str_ip);
-                read = getline(&texto, &len, fptr);
-                printf("TTL: %d, IP: %s id: %s\n", ttl, str_ip, id);
-                if (str_ip[strcspn(str_ip, ",\"")] == ',')
-                {
-                    char otrosIPs[300] = {'\0'};
-                    for (int z = 0; z < strlen(texto) - 6; z++)
-                        otrosIPs[z] = texto[z + 1];
-                    short s = strcspn(otrosIPs, "\"");
-                    otrosIPs[s++] = ',';
-                    otrosIPs[s++] = ' ';
-                    for (int z = 0; z < strcspn(str_ip, ",") && (isdigit(str_ip[z]) || str_ip[z] == '.') ; z++)
-                        otrosIPs[s + z] = str_ip[z];
-                    printf("Para actualizar la base: %s\n", otrosIPs);
-                    // hacer update en elasticsearch
-                    char *datapost = malloc(MAXSIZE);
-                    sprintf(datapost, "curl -X POST \"elasticsearch:9200/zones/_doc/%s/_update?pretty\" -H 'Content-Type: application/json' -d '{\"doc\": {\"IP\": \"%s\"}}' >> elasticsearch.json", id, otrosIPs);
-
-                    int status = system(datapost);
-                    printf("curl status: %d\n", status);
-                }
-
-                str_ip[strcspn(str_ip, ",\"")] = '\0';
-
-                generar_paquete(buffer, bytes_read, ttl, inet_addr(str_ip));
-
-                fptr = fopen("respuestaDNS.bin", "wb");
-                fwrite(buffer, 1, bytes_read + 16, fptr);
-                fclose(fptr);
-
-                printf("Se resolvió sin ir al api.\n");
-
-                sendto(sock, buffer, bytes_read + 16, 0, (struct sockaddr *)&client_addr, addr_len);
-
-                fflush(stdout);pthread_mutex_unlock(&mutex);
-                return NULL;
-            }
-            fclose(fptr);
+            for (int j = i + 1; j <= i + buffer[i]; j++)
+                hostname[k++] = buffer[j];
+            i += buffer[i] + 1;
+            if (buffer[i] != 0)
+                hostname[k++] = '.';
         }
 
-        // el encode en base64 se guarda en 'enc'
-        // Fuente del codigo: https://nachtimwald.com/2017/11/18/base64-encode-and-decode-in-c/
+        printf("Domain name: %s\n", hostname);
 
-        enc = b64_encode(buffer, bytes_read);
-        printf("encoded: '%s'\n", enc);
+        char *dataget = malloc(MAXSIZE);
+        sprintf(dataget, "http://elasticsearch:9200/zones/_doc/_search?q=hostname:%s", hostname);
 
-        // char *urllink = "http://localhost:443/api/dns_resolver";
-        char *datapost = malloc(MAXSIZE);
-        sprintf(datapost, "http://restapi:443/api/dns_resolver?data=%s", enc);
-
-        fptr = fopen("to_decode.txt", "wb");
-        if (!fptr)
-        {
-            fprintf(stderr, "Could not open output file.\n");
-        }
+        fptr = fopen("elasticsearch.json", "w");
 
         CURL *curl;
         CURLcode res;
@@ -396,8 +294,8 @@ void *thread_function(void *arg){
         curl = curl_easy_init();
         if (curl)
         {
-            curl_easy_setopt(curl, CURLOPT_URL, datapost);
-            curl_easy_setopt(curl, CURLOPT_POST, 1L);
+            curl_easy_setopt(curl, CURLOPT_URL, dataget);
+            curl_easy_setopt(curl, CURLOPT_HTTPGET, 1L);
             curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, writeCallback);
             curl_easy_setopt(curl, CURLOPT_WRITEDATA, (void *)fptr);
             res = curl_easy_perform(curl);
@@ -406,37 +304,142 @@ void *thread_function(void *arg){
                         curl_easy_strerror(res));
             curl_easy_cleanup(curl);
         }
-        curl_global_cleanup();        
+        curl_global_cleanup();
+
         fclose(fptr);
+        // if resultado ==
+        fptr = fopen("elasticsearch.json", "r");
 
-        fptr = fopen("to_decode.txt", "r"); // Modo lectura
-        char todecode[MAXSIZE];                     // Aquí vamos a ir almacenando cada línea
-        fgets(todecode, MAXSIZE, fptr);
+        char str_ip[17] = {'\0'};
+        char id[21] = {'\0'};
+        int ttl;
+
+        size_t len = 0;
+        char *texto;
+        ssize_t read;
+
+        read = getline(&texto, &len, fptr);
+        if (texto[2] == 't' && texto[strcspn(texto, "[]") + 1] != ']')
+        { // Esto quiere decir que hay matches
+            fseek(fptr, 0, SEEK_SET);
+            while (1)
+                if (fgetc(fptr) == ',' && fgetc(fptr) == '"')
+                {
+                    if ((texto[0] = fgetc(fptr)) == '_' && fgetc(fptr) == 'i' && fgetc(fptr) == 'd' && fgetc(fptr) && fgetc(fptr) && fgetc(fptr))
+                        fgets(id, 21, fptr);
+                    else if (texto[0] == 'T')
+                        break;
+                }
+
+            fscanf(fptr, "TL\": \"%d\",\"IP\":", &ttl); //     "TTL": "[0-9]+",
+            texto[0] = fgetc(fptr);
+            if (texto[0] == ' ')
+                fscanf(fptr, "\"%s\"", str_ip);
+            else // texto[0] debería ser una comilla
+                fscanf(fptr, "%s\"", str_ip);
+            read = getline(&texto, &len, fptr);
+            printf("TTL: %d, IP: %s id: %s\n", ttl, str_ip, id);
+            if (str_ip[strcspn(str_ip, ",\"")] == ',')
+            {
+                char otrosIPs[300] = {'\0'};
+                for (int z = 0; z < strlen(texto) - 6; z++)
+                    otrosIPs[z] = texto[z + 1];
+                short s = strcspn(otrosIPs, "\"");
+                otrosIPs[s++] = ',';
+                otrosIPs[s++] = ' ';
+                for (int z = 0; z < strcspn(str_ip, ",") && (isdigit(str_ip[z]) || str_ip[z] == '.'); z++)
+                    otrosIPs[s + z] = str_ip[z];
+                printf("Para actualizar la base: %s\n", otrosIPs);
+                // hacer update en elasticsearch
+                char *datapost = malloc(MAXSIZE);
+                sprintf(datapost, "curl -X POST \"elasticsearch:9200/zones/_doc/%s/_update?pretty\" -H 'Content-Type: application/json' -d '{\"doc\": {\"IP\": \"%s\"}}' >> elasticsearch.json", id, otrosIPs);
+
+                int status = system(datapost);
+                printf("curl status: %d\n", status);
+            }
+
+            str_ip[strcspn(str_ip, ",\"")] = '\0';
+
+            generar_paquete(buffer, bytes_read, ttl, inet_addr(str_ip));
+
+            fptr = fopen("respuestaDNS.bin", "wb");
+            fwrite(buffer, 1, bytes_read + 16, fptr);
+            fclose(fptr);
+
+            printf("Se resolvió sin ir al api.\n");
+
+            sendto(sock, buffer, bytes_read + 16, 0, (struct sockaddr *)&client_addr, addr_len);
+
+            fflush(stdout);
+            pthread_mutex_unlock(&mutex);
+            return NULL;
+        }
         fclose(fptr);
+    }
 
-        out_len = b64_decoded_size(todecode);
-        unsigned char out[out_len];
-        // out = malloc(out_len);
+    // el encode en base64 se guarda en 'enc'
+    // Fuente del codigo: https://nachtimwald.com/2017/11/18/base64-encode-and-decode-in-c/
 
-        if (!b64_decode(todecode, out, out_len))
-        {
-            printf("Decode Failure\n");
-        }
+    enc = b64_encode(buffer, bytes_read);
+    printf("encoded: '%s'\n", enc);
 
-        // lo guarda en un txt para comprobar que no hay perdida de datos
-        fptr = fopen("respuestaDNS.bin", "wb");
-        fwrite(out, 1, out_len, fptr);
-        fclose(fptr);
-        if (sendto(sock, out, out_len, 0, (struct sockaddr *)&client_addr, addr_len) == -1)
-        {
-            printf("Error: sendto()!!!!!!!!!!!!!!!\n");
-        }
-        else
-        {
-            printf("El api resolvió con éxito.\n");
-        }
+    // char *urllink = "http://localhost:443/api/dns_resolver";
+    char *datapost = malloc(MAXSIZE);
+    sprintf(datapost, "http://restapi:443/api/dns_resolver?data=%s", enc);
 
-        fflush(stdout);
+    fptr = fopen("to_decode.txt", "wb");
+    if (!fptr)
+    {
+        fprintf(stderr, "Could not open output file.\n");
+    }
+
+    CURL *curl;
+    CURLcode res;
+    curl_global_init(CURL_GLOBAL_ALL);
+    curl = curl_easy_init();
+    if (curl)
+    {
+        curl_easy_setopt(curl, CURLOPT_URL, datapost);
+        curl_easy_setopt(curl, CURLOPT_POST, 1L);
+        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, writeCallback);
+        curl_easy_setopt(curl, CURLOPT_WRITEDATA, (void *)fptr);
+        res = curl_easy_perform(curl);
+        if (res != CURLE_OK)
+            fprintf(stderr, "curl_easy_perform() failed: %s\n",
+                    curl_easy_strerror(res));
+        curl_easy_cleanup(curl);
+    }
+    curl_global_cleanup();
+    fclose(fptr);
+
+    fptr = fopen("to_decode.txt", "r"); // Modo lectura
+    char todecode[MAXSIZE];             // Aquí vamos a ir almacenando cada línea
+    fgets(todecode, MAXSIZE, fptr);
+    fclose(fptr);
+
+    out_len = b64_decoded_size(todecode);
+    unsigned char out[out_len];
+    // out = malloc(out_len);
+
+    if (!b64_decode(todecode, out, out_len))
+    {
+        printf("Decode Failure\n");
+    }
+
+    // lo guarda en un txt para comprobar que no hay perdida de datos
+    fptr = fopen("respuestaDNS.bin", "wb");
+    fwrite(out, 1, out_len, fptr);
+    fclose(fptr);
+    if (sendto(sock, out, out_len, 0, (struct sockaddr *)&client_addr, addr_len) == -1)
+    {
+        printf("Error: sendto()!!!!!!!!!!!!!!!\n");
+    }
+    else
+    {
+        printf("El api resolvió con éxito.\n");
+    }
+
+    fflush(stdout);
     pthread_mutex_unlock(&mutex);
 }
 
@@ -466,25 +469,28 @@ int main()
         exit(1);
     }
 
-
     printf("UDPServer Waiting for client on port 53\n");
     fflush(stdout);
 
     while (1)
     {
-        struct sockaddr_in client_addr=*(struct sockaddr_in*)malloc(sizeof(struct sockaddr_in));
+        struct sockaddr_in client_addr = *(struct sockaddr_in *)malloc(sizeof(struct sockaddr_in));
         bytes_read = recvfrom(sock, buffer, 1024, 0, (struct sockaddr *)&client_addr, &addr_len);
-        //Se crean los parámetros para crear el hilo
+        // Se crean los parámetros para crear el hilo
         ThreadArgs *arg = malloc(sizeof(ThreadArgs));
-        arg->buffer = buffer;
+        arg->buffer = malloc(bytes_read);
+        for (int i = 0; i < bytes_read; i++)
+        {
+            arg->buffer[i] = buffer[i];
+        }
+        
         arg->bytes_read = bytes_read;
         arg->client_addr = client_addr;
         arg->addr_len = addr_len;
         arg->sock = sock;
-        //Se crea el hilo
-        pthread_t*thread_id = malloc(sizeof(pthread_t));
+        // Se crea el hilo
+        pthread_t *thread_id = malloc(sizeof(pthread_t));
         pthread_create(thread_id, NULL, (void *)&thread_function, (void *)arg);
-        
     }
     return 0;
 }
